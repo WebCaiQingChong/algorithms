5d815274469772a7bafb566a7bd2b5dce5f19e42
青虫 <qingchong@dian.so>
2018-06-13 15:39:53 +0800

    2018-06-13 解决昨天的问题，新增一个

--- resolveInJS/strRemoveRepeat.js
+++ resolveInJS/strRemoveRepeat.js
@@ -41,15 +41,22 @@
 var lengthOfLongestSubstring = function (s) {
   let [mx, start, chars] = [0, 0, {}]
   for (let i = 0; i < s.length; i++) {
+    // 如果该条件不成立，即chars[s[i]] === undefined ,也就是字符串还未重复
     if (start <= chars[s[i]]) {
       start = chars[s[i]] + 1
     } else {
+      // 字符串不重复，max值累加
       mx = Math.max(mx, i - start + 1)
     }
     chars[s[i]] = i
   }
   return mx
 }
-console.log(lengthOfLongestSubstring('dvdc'))
+console.log(lengthOfLongestSubstring('abcdfffffff'))
 
 // 没写出来，有问题，但是两点了，得睡觉了
+
+// 答案解析：
+// max来保存最大值，start来记录当前的断续之后的长度，chars来记录不重复的字符串，
+// chars对象的key值为出现过的字符串，value表示出现的最后位置
+// max的值来取重复中最大的长度，或者不重复的长度会一直来叠加max
