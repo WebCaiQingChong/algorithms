a8e2917ef1bac7ff03425c43bb0bb9ff85d65069
青虫 <qingchong@dian.so>
2018-07-02 10:36:42 +0800

    daily commit

--- resolveInJS/Roman_to_Integer.js
+++ resolveInJS/Roman_to_Integer.js
@@ -26,9 +26,16 @@
 // Given a roman numeral, convert it to an integer.
 // Input is guaranteed to be within the range from 1 to 3999.
 
+// 思路：
+// 对字符串进行遍历，定义map来管理罗马数字与数字的映射关系
+// 因为罗马数字也是按照从左到右来进行计算的，只不过需要注意其简写形式
+// 从简写形式上来看，如果后面小于前面，我们只需累加，如果后面大于前面，
+// 则说明是退位， 例 MCM = 1000 + 100 + 1000 - 2 * 100 = 1900
+// 所以只需关心后面大于前面的情况，其他情况正常累加
 /**
  * @param {string} s
  * @return {number}
+ * 方案1：直接使用for循环遍历
  */
 var romanToInt = function (s) {
   const numMap = {
@@ -40,4 +47,30 @@ var romanToInt = function (s) {
     'D': 500,
     'M': 1000
   }
+  let init = 0
+  for (let i = 0, len = s.length; i < len; i++) {
+    if (numMap[s[i]] > numMap[s[i - 1]]) {
+      init = init + numMap[s[i]] - 2 * numMap[s[i - 1]]
+    } else {
+      init += numMap[s[i]]
+    }
+  }
+  return init
 }
+// console.log(romanToInt('MCMXCIV'))
+// 使用reduce，来简化代码
+
+var romanToIntReduce = function (s) {
+  const numMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+  return s.split('')
+          .reduce((total, current, index, arr) => total + numMap[current] - (numMap[arr[index]] > numMap[arr[index - 1]] ? 2 * numMap[arr[index - 1]] : 0), 0)
+}
+console.log(romanToIntReduce('MCMXCIV'))
