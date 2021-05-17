fee93441b987c588e7421f682bc485dea061e673
青虫 <qingchong@dian.so>
2021-05-18 00:25:25 +0800

    daily  commit

---
+++ resolveInJS/路径总和.js
@@ -0,0 +1,38 @@
+// 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+
+// 叶子节点 是指没有子节点的节点。
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+ var hasPathSum = function(root, targetSum) {
+    if (!root) {
+        return false
+    }
+    if (!root.left && !root.right) {
+        return root.val === targetSum
+    }
+    function traverse (node, sum) {
+        if (!node) {
+            return false
+        }
+        if (!node.left && !node.right) {
+            return sum + node.val === targetSum
+        } else {
+            const current = sum + node.val
+            return traverse(node.left, current) || traverse(node.right, current)
+        }
+        
+    }
+
+    return traverse(root.left, root.val) || traverse(root.right, root.val)
+};
