0020ed952b6670b03074698a38153fcd2361d086
青虫 <qingchong@dian.so>
2021-07-12 00:06:39 +0800

    算法打卡第五天

---
+++ resolveInJS/删除链表的倒数第 N 个结点.js
@@ -0,0 +1,51 @@
+// 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+// 进阶：你能尝试使用一趟扫描实现吗？
+
+//  
+
+// 示例 1：
+
+
+// 输入：head = [1,2,3,4,5], n = 2
+// 输出：[1,2,3,5]
+// 示例 2：
+
+// 输入：head = [1], n = 1
+// 输出：[]
+// 示例 3：
+
+// 输入：head = [1,2], n = 1
+// 输出：[1]
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ var removeNthFromEnd = function(head, n) {
+    let dumny = new ListNode(0 , head)
+
+    let first = head
+    let second = dumny
+
+    for (let i = 0; i < n; i++) {
+        first = first.next
+    }
+    
+    while (first) {
+        first = first.next
+        second = second.next
+    }
+
+    second.next = second.next.next
+
+    return dumny.next
+};
